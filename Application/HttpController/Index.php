a144ff0dab8f698dbfc6a60f69a895a973932681
安 <394378627@qq.com>
2018-04-09 22:49:49 +0800

    测试

--- Application/HttpController/Index.php
+++ Application/HttpController/Index.php
@@ -9,7 +9,7 @@
 namespace App\HttpController;
 
 
-use App\Model\User;
+use App\Model\Manager;
 use EasySwoole\Core\Http\AbstractInterface\Controller;
 
 class Index extends Controller
@@ -17,14 +17,8 @@ class Index extends Controller
 
     function index()
     {
-        $openid = str_random(16);
-        $user = new User();
-        if (!$user->has_join(1, $openid)) {
-            $data['username'] = str_random(6);
-            $data['openid'] = $openid;
-            $data['active_id'] =1;
-            $user_id = $user->add($data);
-        }
+        $manager = new Manager();
+        $clients = $manager->get_managers_clients(1);
         $content = file_get_contents(__DIR__ . '/user.html');
         $this->response()->write($content);
     }
