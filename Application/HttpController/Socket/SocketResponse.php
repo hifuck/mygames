05995b0a262222c8a2fadb4a0d95a0c808442399
安 <394378627@qq.com>
2018-04-19 19:03:08 +0800

    test

--- Application/HttpController/Socket/SocketResponse.php
+++ Application/HttpController/Socket/SocketResponse.php
@@ -14,10 +14,10 @@ class SocketResponse
 {
     static function response($fd, $data = [], $msg = '', $code = 200, int $opCode = 1, bool $finish = true)
     {
-//        $res['code'] = $code;
-//        $res['msg'] = $msg;
-//        $res['data'] = $data;
-        $data = json_encode($data, 256);
+        $res['code'] = $code;
+        $res['msg'] = $msg;
+        $res['data'] = $data;
+        $data = json_encode($res, 256);
         $server = ServerManager::getInstance()->getServer();
         return $server->push($fd, $data, $opCode, $finish);
     }
