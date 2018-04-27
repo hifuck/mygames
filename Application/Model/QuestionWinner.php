a5b46956eeecbfe0d585e0e64910a002fa8c26fa
安智超 <394378627@qq.com>
2018-04-27 17:27:06 +0800

    coding

--- Application/Model/QuestionWinner.php
+++ Application/Model/QuestionWinner.php
@@ -13,8 +13,19 @@ class QuestionWinner extends BaseModel
 {
     protected $tableName = 'question_winners';
 
-    public function add($data)
+    public function add($winner)
     {
-        return $this->db->insert($this->tableName, $data);
+        if (!$this->findWinner($winner)) {
+            return $this->db->insert($this->tableName, $winner);
+        }
+        return false;
+    }
+
+    public function findWinner($winner)
+    {
+        $active_id = $winner['active_id'];
+        $round_number = $winner['round_number'];
+        $user_id = $winner['user_id'];
+        return $this->db->where('active_id', $active_id)->where('round_number', $round_number)->where('user_id', $user_id)->getOne($this->tableName);
     }
 }
